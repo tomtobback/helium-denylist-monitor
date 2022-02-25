20608293e2159f4fa21713063b849ca0255c8670
Tom Tobback <tomtobback@gmail.com>
2022-02-25 12:53:52 +0800

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# helium-denylist-monitor
-script to track changes in the Helium denylist
+# Helium denylist Monitor
+
+## Background
+
+Location spoofing has been a problem for the Helium network from the start. Scammers are tricking the Proof-of-Coverage algorithm and earning big rewards for their hotspots while everyone knows what’s going on. Helium’s (temporary?) solution, launched in January 2022, is a [denylist](https://github.com/helium/denylist) with known fake hotspots, relying on the public to request Additions or Removals to the list. The list is growing rapidly, with over a thousand [open issues](https://github.com/helium/denylist/issues). Currently close to 7% of [all Helium hotspots](https://explorer.helium.com/) are on the list (25/2/2022).
+
+## Monitoring the denylist
+
+![](20220225_Helium_denylist_github_commits.png)
+The Helium team processes the Addition/Removal issues in batches, and has been releasing new denylists every week or so. This [script](monitor_denylist.py) tracks the changes by cloning the github repository, and loading the different versions of the denylist to compare with the previous version.
+
+* Previous: hotspots already included in the previous denylist
+* Additions: hotspots newly added (relative to the previous denylist) 
+* Removals: hotspots removed from the previous denylist (so few that the don't show up yet on the bar graph)
+
+Status on 25 Feb 2022 (as in above bar graph)
+
+|hash|date|size|new|old|removed|name|
+|:---|:---|---:|---:|---:|---:|:---|
+| b52ad8e | 2022-01-20 | 1 | 0 | 0 | 0 | initial README |
+| 8ec9385 | 2022-01-24 | 1 | 0 | 0 | 0 | Initial Instructions for maintainers/signers (#3) |
+| 69b8e1c | 2022-01-28 | 25,047 | 25,047 | 0 | 1 | 2022012601 (#21) |
+| 0b2b24e | 2022-02-02 | 26,272 | 1,256 | 25,016 | 31 | 2022020201 (#170) |
+| 78e34c2 | 2022-02-05 | 26,273 | 1 | 26,272 | 0 | 2022020501 (#339) |
+| 18a64b8 | 2022-02-08 | 29,629 | 3,377 | 26,252 | 21 | Updates to the denylist (#409) |
+| 5545cb2 | 2022-02-12 | 40,484 | 10,855 | 29,629 | 0 | 2022021201 (#684) |
+| d15fae0 | 2022-02-22 | 41,544 | 1,060 | 40,484 | 0 | 20220222 (#1474) |
+
+## Installation
+To use this script, you will have to clone the Helium [denylist](https://github.com/helium/denylist) repo, and point the `GIT_DIR` variable in the script to your denylist folder.
+
+The script starts with `git pull` to update your local denylist repo. Then it looks for the different commits of `denylist.csv` and loads them in a list to compare each denylist with the previous one.
+
+Note that this does NOT access the Helium blockchain API; the denylist is only used in the Proof-of-Coverage system running on hotspots, at the discretion of hotspot manufacturers.
+
+## Disclaimer
+It is very well possible that mistakes were made in the script and in the above interpretation, I am open to corrections and suggestions.
+
+I got interested in Helium when I saw dozens of hotspots appearing in my area, which all turned out to be fake. I am trying to contribute to [mapping](https://cassiopeia.hk/finding-real-helium-hotspots-in-hong-kong) of real Helium network coverage in Hong Kong, and I remain a big fan of The Things Network.
+
+
+
---
+++ monitor_denylist.py
@@ -0,0 +1,131 @@
+# this script needs a git clone of https://github.com/helium/denylist
+# insert the .git directory in GIT_DIR
+
+#    MIT LICENSE
+#    Copyright (c) 2022 Tom Tobback
+#    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+#    to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+#    IN THE SOFTWARE.
+
+import os
+import subprocess
+from operator import itemgetter
+import matplotlib.pyplot as plt
+
+GIT_DIR = "/home/tom/CODE/lorawan/helium/github-helium-denylist/denylist"
+
+print("=================================================")
+print("ANALYSE DENYLIST VIA GIT CLONE")
+print("=================================================")
+
+print("looking in folder", GIT_DIR)
+os.chdir(GIT_DIR)
+
+print("updating repo with git pull..")
+os.system("git pull")
+
+# get output of git log into a string
+print("checking all commits of denylist")
+commits_str = subprocess.check_output("git log --pretty=format:\"%h,%ad,%s\" --date=short denylist.csv", shell=True).decode("utf-8")
+
+# initialise list for commits
+commit_list = []
+# process csv string into lines and split into fields
+commit_lines = commits_str.splitlines()
+for line in commit_lines:
+    line_fields = line.split(",")
+    #print(line_fields)
+    commit_list.append(line_fields)
+
+sorted_commit_list = sorted(commit_list, key=itemgetter(1))
+        
+# initialise list for denylists and counts
+denylist_list = []
+denylist_counts_list = []
+for index, commit in enumerate(sorted_commit_list):
+    this_denylist_str = subprocess.check_output("git show " + commit[0] + ":denylist.csv", shell=True).decode("utf-8")
+    this_denylist = this_denylist_str.splitlines()
+    # check difference with previous list
+    new_count = 0
+    old_count = 0
+    removed_count = 0
+    if index > 1:
+        previous_denylist = denylist_list[index-1]
+        for hotspot in this_denylist:
+            if hotspot not in previous_denylist:
+                new_count += 1
+        #print("new:", new_count)
+        # we have the new count, so the rest of this list is old count
+        old_count = len(this_denylist) - new_count
+        # to find out the removed count, we look at the previous list
+        removed_count = len(previous_denylist) - old_count
+    # add this list to list of lists
+    denylist_list.append(this_denylist)
+    this_commit_dict = {}
+    this_commit_dict['hash'] = commit[0]
+    this_commit_dict['date'] = commit[1]    
+    this_commit_dict['name'] = commit[2]    
+    this_commit_dict['size'] = len(this_denylist)
+    this_commit_dict['new'] = new_count
+    this_commit_dict['old'] = old_count
+    this_commit_dict['removed'] = removed_count
+    print(this_commit_dict['hash'], this_commit_dict['date'], 
+    "\tsize:", this_commit_dict['size'],
+    "\tnew:", this_commit_dict['new'],
+    "\told:", this_commit_dict['old'],
+    "\tremoved:", this_commit_dict['removed'],
+    "\tname:", this_commit_dict['name'])  
+    denylist_counts_list.append(this_commit_dict)
+
+
+# plot bar graph
+plt.figure(num="Helium denylist github commits")    
+dates = []
+sizes = []
+news = []
+olds = []
+removeds = []
+print("=================================================")
+print("markdown friendly format")
+print("=================================================")
+for denylist_count in denylist_counts_list:
+    dates.append(denylist_count['date'])
+    sizes.append(denylist_count['size'])
+    news.append(denylist_count['new'])
+    olds.append(denylist_count['old'])        
+    removeds.append(-denylist_count['removed']) # to show negative bar
+    # print in markdown friendly format
+    print("|", denylist_count['hash'], "|", denylist_count['date'], 
+    "|",denylist_count['size'], "|",denylist_count['new'], "|",
+    denylist_count['old'], "|",denylist_count['removed'], "|",
+    denylist_count['name'], "|")
+plt.grid(axis='y', alpha=0.3, linestyle='dashed')
+# add bars
+plt.bar(dates, olds, color='darkblue')
+plt.bar(dates, news, bottom=olds, color='royalblue') # stacked
+plt.bar(dates, removeds, color='violet') # negative
+plt.xlabel("commit dates")
+plt.ylabel("hotspot counts")
+# format y label with 1,000 separator
+y_values = plt.gca().get_yticks()
+plt.gca().set_yticklabels(['{:,.0f}'.format(y) for y in y_values])
+plt.title("Helium denylist github commits")
+plt.legend(labels=["Previous", "Additions", "Removals"])
+# print size labels on top of bars
+# look for max size to set the ylim with margin for size labels
+max_size = 0
+for x, size in enumerate(sizes):
+    plt.text(x, size + 1000, f"{size:,d}", color='blue', ha='center')
+    if size > max_size:
+        max_size = size
+plt.ylim(top=max_size + 5000)
+plt.axhline(y=0, color='black', linewidth=0.5)
+plt.show()    
+
+print("=================================================")
+
